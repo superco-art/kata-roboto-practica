2f331edc26fbc91c325a31c6dcc33a38961948b2
superco-art <daniela.trifu@gmail.com>
2022-01-19 18:17:25 +0100

    utilizando regex para ciudad

--- src/main/java/Roboto.java
+++ src/main/java/Roboto.java
@@ -1,7 +1,16 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Roboto {
 
     public String responder(String frase){
-        return "hola humano!";
+
+        Pattern pattern = Pattern.compile("Que tiempo hace en (\\w+)\\?");
+        Matcher matcher = pattern.matcher(frase);
+        if(matcher.find()){
+            return "Te vas a " +matcher.group(1)+ " y lo miras.";
+        }
+        return "Hola humano!";
     }
 
 }
--- src/test/java/testRoboto.java
+++ src/test/java/testRoboto.java
@@ -10,7 +10,25 @@ class testRoboto {
      public void saludoRespuesta(){
         Roboto roboto = new Roboto();
           String respuesta = roboto.responder("Hola!");
-          assertThat(respuesta, equalTo("hola humano!"));
+          assertThat(respuesta, equalTo("Hola humano!"));
+
+    }
+
+    @Test
+
+    public void respondeSiLePreguntoQueTiempoHace(){
+        Roboto roboto = new Roboto();
+        String respuesta = roboto.responder("Que tiempo hace en Barcelona?");
+        assertThat(respuesta, equalTo("Te vas a Barcelona y lo miras."));
+
+    }
+
+    @Test
+
+    public void respondeSiLePreguntoQueTiempoHaceEnOtraCiudad(){
+        Roboto roboto = new Roboto();
+        String respuesta = roboto.responder("Que tiempo hace en Madrid?");
+        assertThat(respuesta, equalTo("Te vas a Madrid y lo miras."));
 
     }
 
